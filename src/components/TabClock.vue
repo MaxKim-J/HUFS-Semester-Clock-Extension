63680ec054c00bab0389c0906ce3e9593d009f84
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-11-29 17:30:00 +0900

    tabClock.vue 기본적인 레이아웃 스타일 작업

--- src/components/TabClock.vue
+++ src/components/TabClock.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="tab-clock">
     <div class="tab-clock-main">
-      <div class="tab-clock-main-title">{{this.semesterInfo}}학기 종강까지 남은시간</div>
+      <div class="tab-clock-main-title">{{this.semesterInfo}}학기 종강까지</div>
       <div
         class="tab-clock-main-contents"
-        ref="mainclock"
-        v-cloak
       >{{ this.daysCalculated }}일 {{ this.hoursCalculated }}시간 {{ this.minutesCalculated }}분 {{ this.secondsCalculated }}초</div>
+      <div class="tab-clock-main-title">남았습니다</div>
     </div>
-    <div class="tab-clock-expired">종강 : {{ this.semesterExpired | moment("YYYY년 MM월 DD일") }}</div>
-    <div class="tab-clock-today">오늘 : {{ this.today | moment("YYYY년 MM월 DD일") }}</div>
+    <div class="tab-clock-info">종강 : {{ this.semesterExpired | moment("YYYY년 MM월 DD일") }}</div>
+    <div class="tab-clock-info">오늘 : {{ this.today | moment("YYYY년 MM월 DD일") }}</div>
   </div>
 </template>
 
@@ -26,38 +25,59 @@ export default {
   },
   computed: {
     secondsCalculated () {
-      return this.gapTime % 60
+      return this.addZero(this.gapTime % 60)
     },
     minutesCalculated () {
-      return parseInt((this.gapTime / 60) % 60)
+      return this.addZero(parseInt((this.gapTime / 60) % 60))
     },
     hoursCalculated () {
-      return parseInt((this.gapTime / (60 * 60)) % 24)
+      return this.addZero(parseInt((this.gapTime / (60 * 60)) % 24))
     },
     daysCalculated () {
-      return parseInt((this.gapTime / (3600 * 24)) % 24)
+      return this.addZero(parseInt((this.gapTime / (3600 * 24)) % 24))
     }
   },
   methods: {
     getDueDates () {
       const today = new Date()
       this.gapTime = parseInt((this.semesterExpired - today) / 1000)
+    },
+    addZero (num) {
+      if (num < 10) {
+        return `0${num}`
+      } else {
+        return `${num}`
+      }
     }
   },
   created () {
     this.getDueDates()
   },
   mounted () {
-    this.interval = setInterval(() => this.getDueDates(), 1000)
+    this.interval = setInterval(this.getDueDates, 1000)
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .tab-clock {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   .tab-clock-main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 20px 0px;
     .tab-clock-main-title {
+      font-size: 20px;
     }
+    .tab-clock-main-contents {
+      font-size: 75px;
+    }
+  }
+  .tab-clock-info {
+    font-size: 15px;
   }
 }
 </style>
