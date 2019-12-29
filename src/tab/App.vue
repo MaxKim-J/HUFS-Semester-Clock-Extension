67ccd89691fe84958d8c0170ee3f2fc3727cf61b
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-12-30 01:38:00 +0900

    앱 설치시 초기화면 tabInit 컴포넌트로 구현, chrome storage api 이용하여 유저 이름, 학번 입력 받기 구현

--- src/tab/App.vue
+++ src/tab/App.vue
@@ -5,6 +5,9 @@
       <tab-middle></tab-middle>
       <tab-hotlinks></tab-hotlinks>
     </div>
+    <div class="tab-init">
+      <tab-init></tab-init>
+    </div>
     <div class="background">
       <img src="http://ppcdn.500px.org/75319705/1991f76c0c6a91ae1d23eb94ac5c7a9f7e79c480/2048.jpg" />
     </div>
@@ -15,13 +18,27 @@
 import TabClock from '../components/TabClock.vue';
 import TabMiddle from '../components/TabMiddle.vue';
 import TabHotlinks from '../components/TabHotlinks.vue';
+import TabInit from '../components/TabInit.vue';
 
 export default {
   name: 'App',
   components: {
     TabClock,
     TabMiddle,
-    TabHotlinks
+    TabHotlinks,
+    TabInit
+  },
+  data () {
+    return {
+      isUserInit: null
+    }
+  },
+  beforecreated () {
+    chrome.storage.local.get(['userInitInput'], function (result) {
+      console.log(result)
+      this.isUserInit = result.userInitInput
+      console.log(this.isUserInit)
+    })
   }
 }
 </script>
