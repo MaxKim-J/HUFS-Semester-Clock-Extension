bf5aa4ec671a6044e0e742e5abc3ae46307d2b01
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-12-31 10:03:18 +0900

    vue-router 제거, 비동기 컴포넌트로 다시 구현 예정

--- src/tab/tab.js
+++ src/tab/tab.js
@@ -1,14 +1,7 @@
 import Vue from 'vue'
 import App from './App'
-import VueRouter from 'vue-router'
-import Routes from '../../routes.js'
 
-Vue.use(VueRouter)
 Vue.use(require('vue-moment'))
-
-const router = new VueRouter({
-  routes: Routes
-})
 
 new Vue({
   el: '#app',
