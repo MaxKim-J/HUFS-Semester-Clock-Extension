954d3d4b242a6b3586100b11ee192e6cc4988ca1
Max Jonghyuk Kim <hwaseen@gmail.com>
2020-01-04 17:40:57 +0900

    종강시계 종료 후 계절학기/개강시계 시작 로직 구현 종강시계 종료 후 다음 d-day로 tabClock 컴포넌트의 this.semesterInfo가 교체되게끔 함

--- src/components/TabClock.vue
+++ src/components/TabClock.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="tab-clock">
     <div class="tab-clock-main">
-      <div class="tab-clock-main-title">{{this.semesterInfo.id}}학기 종강까지</div>
+      <div class="tab-clock-main-title">{{this.semesterInfo.id}}학기 {{this.semesterInfo.act}}까지</div>
       <div
         class="tab-clock-main-contents"
       >{{ this.daysCalculated }}일 {{ this.hoursCalculated }}시간 {{ this.minutesCalculated }}분 {{ this.secondsCalculated }}초</div>
       <div class="tab-clock-main-title">남았습니다</div>
+      <div
+        class="tab-clock-main-btn"
+        v-if="hadSeason === true"
+        @click="this.drawNextSemester"
+      >계절학기를 안들어요!</div>
     </div>
-    <div class="tab-clock-info">종강 : {{ this.semesterInfo.due | moment("YYYY년 MM월 DD일") }}</div>
-    <div class="tab-clock-info">오늘 : {{ this.today | moment("YYYY년 MM월 DD일") }}</div>
+    <div class="tab-clock-info">오늘 : {{ this.today | moment("YYYY년 MM월 DD일 h:mm a") }}</div>
   </div>
 </template>
 
 <script>
-import { CURRENT_SEMESTER_INFO } from "../utils/SemesterInfo.js";
+import {
+  CURRENT_SEMESTER_INFO,
+  SEASONAL_SEMESTER_INFO,
+  NEXT_SEMESTER_INFO
+} from "../utils/SemesterInfo.js";
 
 import {
   getDistanceSeconds,
@@ -26,7 +34,8 @@ export default {
   name: "TabClock",
   data() {
     return {
-      semesterInfo: CURRENT_SEMESTER_INFO,
+      semesterInfo: null,
+      hadSeason: null,
       today: new Date(),
       gapTime: 0
     };
@@ -46,17 +55,36 @@ export default {
     }
   },
   methods: {
+    clockValid() {
+      const today = new Date();
+      if (today < CURRENT_SEMESTER_INFO.due) {
+        this.semesterInfo = CURRENT_SEMESTER_INFO;
+      } else if (today > CURRENT_SEMESTER_INFO.due) {
+        this.semesterInfo = SEASONAL_SEMESTER_INFO;
+        this.hadSeason = true;
+      }
+    },
     getDueDates() {
       const today = new Date();
-      console.log(this.semesterInfo);
       this.gapTime = parseInt(this.semesterInfo.due - today);
+    },
+    getNowDates() {
+      this.today = new Date();
+    },
+    drawNextSemester() {
+      this.hadSeason = false;
+      this.semesterInfo = NEXT_SEMESTER_INFO;
     }
   },
   created() {
+    this.clockValid();
     this.getDueDates();
   },
   mounted() {
-    this.interval = setInterval(this.getDueDates, 1000);
+    this.interval = setInterval(() => {
+      this.getDueDates();
+      this.getNowDates();
+    }, 1000);
   }
 };
 </script>
--- src/utils/SemesterInfo.js
+++ src/utils/SemesterInfo.js
@@ -2,15 +2,18 @@
 
 export const CURRENT_SEMESTER_INFO = {
   id: '2020-1',
-  due: new Date(2020, 6, 3, 23, 59, 59)
+  act: '종강',
+  due: new Date(2020, 0, 4, 16, 26)
 }
 
 export const SEASONAL_SEMESTER_INFO = {
   id: '2020 여름',
+  act: '종강',
   due: new Date(2020, 6, 13, 23, 59, 59)
 }
 
 export const NEXT_SEMESTER_INFO = {
   id: '2020-2',
-  start: new Date(2020, 8, 1, 23, 59, 59)
+  act: '개강',
+  due: new Date(2020, 8, 1, 23, 59, 59)
 }
