8272f93745f9a0aee0f9a9bb348a8c6330450c47
Max Jonghyuk Kim <hwaseen@gmail.com>
2020-01-10 13:36:36 +0900

    footer 배경사진 업로드 이미지 크기 밸리데이션 적용 3MB 이상의 이미지가 업로드 되었을 경우 Promise 안에서 에러 발생시켜 localstorage에 저장 안되게 설정

--- src/components/TabFooter.vue
+++ src/components/TabFooter.vue
@@ -6,6 +6,7 @@
           <div class="tab-side-box">
             <div class="tab-side-box-title">배경화면 바꾸기</div>
             <div class="tab-side-box-content">사진 업로드</div>
+            <div class="tab-side-box-content">파일 업로드는 3MB 까지 가능합니다</div>
             <input type="file" @change="fileToData" />
           </div>
         </div>
@@ -57,16 +58,26 @@ export default {
     },
     handleFileUpload() {
       return new Promise((resolve, reject) => {
+        this.fileSizeValid(event.target.files[0]);
         let fileReader = new FileReader();
         fileReader.readAsDataURL(event.target.files[0]);
         fileReader.onload = () => resolve(fileReader.result);
       });
     },
     fileToData() {
-      this.handleFileUpload().then(data => {
-        this.backgroundImgFile = data;
-        this.$emit("upload", this.backgroundImgFile);
-      });
+      this.handleFileUpload()
+        .then(data => {
+          this.backgroundImgFile = data;
+          this.$emit("upload", this.backgroundImgFile);
+        })
+        .catch(() => {
+          alert("배경화면 이미지는 3MB를 초과할 수 없습니다");
+        });
+    },
+    fileSizeValid(file) {
+      if (file.size > 3000000) {
+        throw new Error("이미지 파일 용량 초과");
+      }
     }
   }
 };
