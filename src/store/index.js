e803137693adaa1f807b402024a8cd20bd89b9dc
Max Jonghyuk Kim <hwaseen@gmail.com>
2020-01-21 21:45:45 +0900

    tabMiddle 컴포넌트에 Vuex 적용 tabMiddle 컴포넌트에서 chrome.localstorage와 통신하는 로직들 Vuex에 옮겨서 구현

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import * as getters from './getters'
-import mutations from './mutations'
-import * as actions from './actions'
+// 뷰엑스 저장소
+// 로컬 스토리지와 통신하는 로직들은 모두 저장소에서 구현했습니다!
 
 Vue.use(Vuex)
 
+import { localStorageGet, localStorageSet, localStorageRemove } from "../services/localStorageAccess"
+
+const dataArr = ["userName", "userFreshmanYear"];
+
 export default new Vuex.Store({
   state: {
-    foo: 'bar'
+    userName: "",
+    userFreshmanYear: 0,
   },
-  getters,
-  mutations,
-  actions
+  mutations: {
+    GET_USER_INFO(state, payload) {
+      state.userName = payload.userName
+      state.userFreshmanYear = payload.userFreshmanYear
+    },
+    REMOVE_USER_INFO(state) {
+      state.userName = ""
+      state.userFreshmanYear = 0
+      localStorageRemove(dataArr)
+    },
+    UPDATE_USER_INFO(state, payload) {
+      state.userName = payload.userName
+      state.userFreshmanYear = payload.userFreshmanYear
+      localStorageSet(payload)
+    }
+  },
+  actions: {
+    getUserInfo({ commit }) {
+      localStorageGet(dataArr).then(data => {
+        const userInfoData = {
+          userName: data.userName,
+          userFreshmanYear: data.userFreshmanYear,
+        }
+        commit('GET_USER_INFO', userInfoData)
+      })
+    }
+  }
 })
