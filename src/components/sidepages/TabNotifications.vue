4d1a2397cd33b8ff0e2a4e7079e98627f64cc2f4
김종혁 <hwaseen@gmail.com>
2020-02-24 15:14:06 +0900

    학사공지 탭 스타일 작업

--- src/components/sidepages/TabNotifications.vue
+++ src/components/sidepages/TabNotifications.vue
@@ -1,25 +1,27 @@
 <template>
     <div class="tab-side-box">
         <div class="tab-side-box-title">학사공지</div>
-        <div class="tab-side-box-content"></div>
+        <div class="tab-side-box-content">✅ 최신 학사공지 15개를 보여드립니다. 항목을 누르시면 링크로 이동해요!</div>
         <div class="tab-notification-box">
-            <div class="tab-notifications-list" v-for="(noti, index) in this.notificationArray" :key="index">
-                <div class="tab-notification" v-if="pageIndex <= noti.id && noti.id < pageIndex + 5">
-                    <span class="tab-notification-date">{{ index }}</span>
+            <div class="tab-notification" v-for="(noti, index) in this.notificationArray" :key="index">
+                <div class="tab-notification-content" v-if="pageIndex <= noti.id && noti.id < pageIndex + 5">
+                    <span class="tab-notification-date">{{ index }}.</span>
                     <span class="tab-notificaion-title"
                         ><a :href="noti.link">{{ noti.title }}</a></span
                     >
                 </div>
             </div>
-            <div class="tab-notification-pagenation">
-                <span
-                    class="tab-notification-pagenation-number"
-                    v-for="(pageNum, index) in notificationArrayCalculated"
-                    :key="index"
-                    @click="changePageIndex(index)"
-                    >{{ pageNum }}</span
-                >
-            </div>
+        </div>
+        <div class="tab-notification-pagenation">
+            <span class="tab-side-box-content-small">페이지 이동 : </span>
+            <span
+                class="tab-notification-pagenation-number tab-side-box-content-small"
+                :class="[checkedPageIndex === index ? 'tab-notification-pagenation-number-checked' : '']"
+                v-for="(pageNum, index) in notificationArrayCalculated"
+                :key="index"
+                @click="changePageIndex(index)"
+                >{{ pageNum }}</span
+            >
         </div>
     </div>
 </template>
@@ -32,7 +34,8 @@ export default {
     data() {
         return {
             notificationArray: [],
-            pageIndex: 0
+            pageIndex: 0,
+            checkedPageIndex: 0
         };
     },
     computed: {
@@ -45,6 +48,7 @@ export default {
     methods: {
         changePageIndex(num) {
             this.pageIndex = num * 5;
+            this.checkedPageIndex = num;
         }
     },
     created() {
@@ -59,5 +63,23 @@ export default {
 <style lang="scss" scoped>
 .tab-notification-box {
     font-size: 14px;
+    margin: 10px 0px;
+    .tab-notification:hover {
+        text-decoration: underline !important;
+    }
+}
+.tab-notification-pagenation {
+    font-size: 14px;
+    .tab-notification-pagenation-number {
+        margin-right: 10px;
+        padding: 1px 4px;
+        border: 1px solid white;
+        border-radius: 15px;
+        cursor: pointer;
+    }
+    .tab-notification-pagenation-number-checked {
+        color: black;
+        background-color: white;
+    }
 }
 </style>
