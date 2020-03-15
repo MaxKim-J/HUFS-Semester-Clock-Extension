dba52b21f3b357e471dfcb0ca285fbe7cb6a707d
김종혁 <hwaseen@gmail.com>
2020-03-15 17:16:41 +0900

    글로벌캠 날씨 조회 기능 추가

--- src/components/sidepages/TabWeather.vue
+++ src/components/sidepages/TabWeather.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="tab-side-box">
-    <div class="tab-side-box-title">이문동 날씨</div>
-    <div class="tab-side-box-content">🌈5일 간의 이문동 날씨와 최저/최고기온입니다.</div>
-    <div class="tab-weather-box">
-      <div class="tab-weather" v-for="(weather, index) in weatherArray" :key="weather.id">
+    <div class="tab-side-box-title">외대 날씨</div>
+    <div class="tab-side-box-content">
+      🌈5일 간의 {{weatherStatus}} 날씨와 최저/최고기온입니다.
+      <span
+        class="tab-side-box-sub-btn"
+        @click="this.changeStatus"
+      >{{weatherStatus === "서울캠(이문동)" ? "글캠":"서울캠"}} 날씨 보기</span>
+    </div>
+
+    <div class="tab-weather-box" v-if="weatherStatus === '서울캠(이문동)'">
+      <div class="tab-weather" v-for="(weather, index) in seoulArray" :key="weather.id">
         <div
           class="tab-weather-title"
           :class="[index == 0 ? 'tab-weather-title-today' : '']"
@@ -16,6 +23,22 @@
         <div class="tab-weather-temp">{{ weather.temp }}</div>
       </div>
     </div>
+
+    <div class="tab-weather-box" v-else>
+      <div class="tab-weather" v-for="(weather, index) in globalArray" :key="weather.id">
+        <div
+          class="tab-weather-title"
+          :class="[index == 0 ? 'tab-weather-title-today' : '']"
+        >{{ weather.date }}</div>
+        <div class="tab-weather-icon">
+          {{ changeToEmoji(weather.morning_icon) }}/{{
+          changeToEmoji(weather.afternoon_icon)
+          }}
+        </div>
+        <div class="tab-weather-temp">{{ weather.temp }}</div>
+      </div>
+    </div>
+
     <div class="tab-alarm">*데이터가 표시되지 않는다면, 와이파이 연결을 확인하세요!</div>
   </div>
 </template>
@@ -32,22 +55,40 @@ import {
 export default {
   data() {
     return {
-      weatherArray: []
+      seoulArray: [],
+      globalArray: [],
+      weatherStatus: "서울캠(이문동)"
     };
   },
   methods: {
     changeToEmoji(key) {
       return weatherValid(key);
+    },
+    changeStatus() {
+      if (this.weatherStatus === "서울캠(이문동)") {
+        this.weatherStatus = "글캠(모현면)";
+        localStorageSet({ weatherShow: "global" });
+      } else {
+        this.weatherStatus = "서울캠(이문동)";
+        localStorageSet({ weatherShow: "seoul" });
+      }
     }
   },
   created() {
-    localStorageGet(["weatherInfo"]).then(data => {
+    localStorageGet(["weatherInfo", "weatherShow"]).then(data => {
+      if (data.weatherShow !== undefined) {
+        data.weatherShow === "seoul"
+          ? (this.weatherStatus = "서울캠(이문동)")
+          : (this.weatherStatus = "글캠(모현면)");
+      }
       if (data.weatherInfo) {
-        this.weatherArray = data.weatherInfo;
+        this.seoulArray = data.weatherInfo.slice(0, 5);
+        this.globalArray = data.weatherInfo.slice(5, 10);
       } else {
         getWeatherFromDB()
           .then(data => {
-            this.weatherArray = data.weather;
+            this.seoulArray = data.weather.slice(0, 5);
+            this.globalArray = data.weather.slice(5, 10);
             localStorageSet({ weatherInfo: data.weather });
           })
           .catch(err => {
