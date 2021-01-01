3ecfdcbc95f08a8b409ff4c191821657bfab6c2a
Max Kim <hwaseen@gmail.com>
2021-01-01 20:58:21 +0900

    코로나 현황 탭 마크업 추가

--- src/components/TabHeader/TabHeader.vue
+++ src/components/TabHeader/TabHeader.vue
@@ -23,6 +23,13 @@
       <div class="tab-bar-contents-right">
         <div
           class="tab-bar-content"
+          :class="[this.coronaStatusIsShowing ? 'tab-bar-content-checked' : '']"
+          @click="this.showCoronaSatus"
+        >
+          코로나19 현황
+        </div>
+         <div
+          class="tab-bar-content"
           :class="[this.randomMenuIsShowing ? 'tab-bar-content-checked' : '']"
           @click="this.showRandomMenu"
         >
@@ -49,7 +56,6 @@
           </div>
         </transition>
       </div>
-
       <div class="tab-bar-box-wrap tab-bar-box-right-wrap">
         <transition name="fade">
           <div
@@ -57,6 +63,14 @@
             v-if="this.randomMenuIsShowing"
           >
             <side-random-menu></side-random-menu>
+          </div>
+        </transition>
+        <transition name="fade">
+          <div
+            class="tab-bar-box tab-bar-box-right"
+            v-if="this.coronaStatusIsShowing"
+          >
+            <side-corona-status></side-corona-status>
           </div>
         </transition>
       </div>
@@ -68,6 +82,7 @@
 import SideRandomMenu from "./TabHeaderSide/SideRandomMenu";
 import SideNotifications from "./TabHeaderSide/SideNotifications";
 import SideWeather from "./TabHeaderSide/SideWeather";
+import SideCoronaStatus from './TabHeaderSide/SideCoronaStatus.vue';
 import "../../style/bar.scss";
 
 export default {
@@ -76,28 +91,37 @@ export default {
     SideRandomMenu,
     SideNotifications,
     SideWeather,
+    SideCoronaStatus
   },
   data() {
-    return {
+    return { 
       randomMenuIsShowing: false,
       notificationIsShowing: false,
       weatherIsShowing: false,
+      coronaStatusIsShowing: false
     };
   },
   methods: {
     showNotifications() {
       this.notificationIsShowing = !this.notificationIsShowing;
       this.randomMenuIsShowing = false;
+      this.coronaStatusIsShowing = false;
+    },
+    showWeather() {
+      this.weatherIsShowing = !this.weatherIsShowing;
+      this.randomMenuIsShowing = false;
+      this.coronaStatusIsShowing = false;
     },
     showRandomMenu() {
       this.randomMenuIsShowing = !this.randomMenuIsShowing;
       this.weatherIsShowing = false;
       this.notificationIsShowing = false;
     },
-    showWeather() {
-      this.weatherIsShowing = !this.weatherIsShowing;
-      this.randomMenuIsShowing = false;
-    },
+    showCoronaSatus() {
+      this.coronaStatusIsShowing = !this.coronaStatusIsShowing;
+      this.weatherIsShowing = false;
+      this.notificationIsShowing = false;
+    }
   },
 };
 </script>
---
+++ src/components/TabHeader/TabHeaderSide/SideCoronaStatus.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tab-side-box">
+    <div class="tab-side-box-title">코로나19 현황</div>
+    <div class="tab-side-box-content">
+      5일간의 신종 코로나 바이러스 신규 확진자 추이입니다.
+    </div>
+   
+  </div>
+</template>
+
+<script>
+import "../../../style/sidePage.scss";
+
+export default {
+  name:'SideCoronaStatus'
+}
+</script>
+
+<style>
+
+</style>
